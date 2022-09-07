1b6e78ae63242137a5d1c656e3ef0801a5b3c983
Ethan Davidson <ethanld@outlook.com>
2022-09-07 00:26:32 -0700

    added css and javascript

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Ethan Davidson";
 document.querySelector("body").appendChild(h2);
+
+document.addEventListener("click", displayDate);
+
+function displayDate() {
+  document.getElementById("demo").innerHTML = Date();
+}
